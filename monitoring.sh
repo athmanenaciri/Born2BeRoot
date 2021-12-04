c09e065a87ae9b6da1c281986190a1f918a249d1
Athmane Naciri <anaciri@e2r11p3.1337.ma>
2021-12-04 21:12:51 +0100

    Shit

---
+++ monitoring.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+arc=$(uname -a)
+pcpu=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
+vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
+fram=$(free -m | awk '$1 == "Mem:" {print $2}')
+uram=$(free -m | awk '$1 == "Mem:" {print $3}')
+pram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
+pdisk=$(df -h --total | tail -1 | awk '{printf "%s/%s (%s)\n", $3, $2, $5}')
+cpul=$(mpstat | tail -1 | awk '{printf "%s%%\n", $4+$6}')
+lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
+lvmt=$(lsblk | grep "lvm" | wc -l)
+lvmu=$(if [ $lvmt -eq 0 ]; then echo no; else echo yes; fi)
+ctcp=$(netstat | grep ESTABLISHED | wc -l)
+ulog=$(users | wc -w)
+ip=$(hostname -I)
+mac=$(ip link show | awk '$1 == "link/ether" {print $2}')
+cmds=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+wall "	#Architecture: $arc
+	#CPU physical : $pcpu
+	#vCPU: $vcpu
+	#Memory Usage: $uram/${fram}MB ($pram%)
+	#Disk Usage: $pdisk
+	#CPU load: $cpul
+	#Last boot: $lb
+	#LVM use: $lvmu
+	#Connexions TCP: $ctcp ESTABLISHED
+	#User log: $ulog
+	#Network: IP $ip ($mac)
+	#Sudo: $cmds cmd"
